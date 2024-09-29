66482d5f289ac6c39ca5446f1a2f09e48791dc36
gurpal <gurpalsinghanand@gmail.com>
2024-09-30 02:42:46 +0530

    [Added] Api Docs for all the apis

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ import mongoose from "mongoose"
 import cors from "cors"
 import { listRoutes } from "./routes/list";
 import { quizRoutes } from "./routes/quiz";
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
 
 const app = express();
 const PORT = process.env.port || 8000;
@@ -14,6 +16,8 @@ const options = {
 }
 
 app.use(cors(options))
+const swaggerDocument = YAML.load('./api-docs/swagger.yaml');
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use("/list", listRoutes);
 app.use("/quiz", quizRoutes);
